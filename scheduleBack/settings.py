29ce2f66615c39c1555df4c6950cddd8f494dbed
Barrack-coder <jumabarrack17@gmail.com>
2022-05-04 20:58:38 +0300

    modified setting

--- scheduleBack/settings.py
+++ scheduleBack/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from distutils import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4eu9$6y9+j%l^x9+0-l&u0fkq(+nw0^_$ademwkzxm2vaw8atk'
-
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
